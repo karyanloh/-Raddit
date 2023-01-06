15e3bffc402b3dbfee8f75e3bbacb1636bbc5cd3
Jeffery Hebert <Jeffery.Hebert2@gmail.com>
2023-01-06 15:54:24 -0700

    connected mongodb to fast api

---
+++ user-service/main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from pydantic import BaseModel
+from user_queries import UserQueries
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        os.environ.get("CORS_HOST", "http://localhost:3000")
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/launch-details")
+def launch_details():
+    return {
+        "launch_details": {
+            "year": 2022,
+            "month": 12,
+            "day": "9",
+            "hour": 19,
+            "min": 0,
+            "tz:": "PST"
+        }
+    }
+
+
+class UserIn(BaseModel):
+    email: str
+    password: str
+
+class UserOut(BaseModel):
+    id: str
+    email: str
+
+@app.post('/api/users', response_model=UserOut)
+def create_user(
+    new_user: UserIn,
+    user_queries: UserQueries = Depends(),
+):
+    return user_queries.create_user(new_user)
+
+@app.get('/api/users/{id}', response_model=UserOut)
+def create_user(
+    id: str,
+    user_queries: UserQueries = Depends(),
+):
+    return user_queries.get_user_by_id(id)
---
+++ user-service/user_queries.py
@@ -0,0 +1,25 @@
+import pymongo
+import os
+from bson import ObjectId
+
+dbuser = os.environ['MONGO_USER']
+dbpass = os.environ['MONGO_PASSWORD']
+dbhost = os.environ['WAIT_HOSTS']
+mongodb = os.environ['DATABASE_NAME']
+
+mongo_str = f"mongodb://{dbuser}:{dbpass}@{dbhost}"
+
+client = pymongo.MongoClient(mongo_str)
+
+class UserQueries:
+    def create_user(self, new_user):
+        db = client[mongodb]
+        result = db.users.insert_one(new_user.dict())
+        user = self.get_user_by_id(result.inserted_id)
+        return user
+
+    def get_user_by_id(self, id):
+        db = client[mongodb]
+        result = db.users.find_one({ "_id": ObjectId(id) })
+        result['id'] = str(result['_id']) # ObjectId
+        return result
