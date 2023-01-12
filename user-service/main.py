6ded20f3748de0499318f6497830ee15d9a476a3
minyue <yukilmy520@gmail.com>
2023-01-11 21:18:26 -0800

    organized user_service folders

--- user-service/main.py
+++ user-service/main.py
@@ -1,12 +1,10 @@
-from fastapi import FastAPI, Depends,Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
-from user_queries import UserQueries
-from models import UserIn, UserOut,AccountToken
-from auth import authenticator
+from routers import auth
+from routers import accounts
 
 app = FastAPI()
-app.include_router(authenticator.router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,30 +16,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.post('/api/users', response_model=UserOut)
-def create_user(
-    new_user: UserIn,
-    user_queries: UserQueries = Depends(),
-):
-    new_user.password = authenticator.hash_password(new_user.password)
-    return user_queries.create_user(new_user)
-
-@app.get('/api/users/{id}', response_model=UserOut)
-def get_user_by_id(
-    id: str,
-    user_queries: UserQueries = Depends(),
-):
-    return user_queries.get_user_by_id(id)
-
-@app.get("/token", response_model=AccountToken | None)
-async def get_token(
-    request: Request,
-    account: dict = Depends(authenticator.try_get_current_account_data)
-) -> AccountToken | None:
-    if account and authenticator.cookie_name in request.cookies:
-        return {
-            "access_token": request.cookies[authenticator.cookie_name],
-            "type": "Bearer",
-            "account": account,
-        }
+app.include_router(auth.authenticator.router)
+app.include_router(accounts.router)
