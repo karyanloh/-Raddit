cdaa431b4269ef510d04257b3ed46471e33a621a
Mackenzie Hill <hillmack92@gmail.com>
2023-01-11 21:11:46 -0300

    nav and app ghi shows simple layout-auth and token code commented out

---
+++ ghi/src/login.js
@@ -0,0 +1,14 @@
+function LoginForm() {
+	return (
+	<div className="px-4 py-5 my-5 text-center">
+		<h1 className="display-5 fw-bold">Raddit</h1>
+		<div className="col-lg-6 mx-auto">
+		<p className="lead mb-4">
+			Login
+		</p>
+		</div>
+	</div>
+);
+}
+
+export default LoginForm;
