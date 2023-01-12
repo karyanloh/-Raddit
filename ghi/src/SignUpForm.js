384165c6a74bb9a34a07688756331040e9f7ba46
stephanie <karyansloh@gmail.com>
2023-01-11 22:25:37 -0800

    modified signup form

--- ghi/src/SignUpForm.js
+++ ghi/src/SignUpForm.js
@@ -1,23 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function SignUpForm(props) {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [signupInfo, setSignupInfo] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  useEffect(()=>
+//   async function handleSubmit(e){
+//     e.preventDefault();
+//     const url = "http://localhost:8000/api/users/"
+//     const fetchOptions={
+//         method: 'post',
+//         body: JSON.stringify(signupInfo),
+//         headers:{
+//             "Content-Type": "application/json",
+//         }
+//     }
+//     const response = await fetch(url, fetchOptions)
+//     if(response.ok){
+//         setSignupInfo({ username: "", email: "", password: "" });
+//     }
+//     },[signupInfo]
+  async function signup(username, password, email) {
+    const url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/users/`;
+    const response = await fetch(url, {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        password,
+        email,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    });
+    if (response.ok) {
+      await login(username, password);
+    }
+    return false;
+  }
+  )
 
+  }
     return (
-      <Form
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
+      <form
+        onSubmit={signup}
       >
         <div className="mb-4">
           <label htmlFor="username">Username</label>
           <input
             type="username"
-            onChange={(e) => setUsername(e.target.value)}
+            value= {signupInfo.username}
+            onChange={(e) => setSignupInfo.username(e.target.value)}
             className="form-control"
             id="username"
             aria-describedby="username"
@@ -28,7 +64,8 @@ function SignUpForm(props) {
           <label htmlFor="email">Email address</label>
           <input
             type="email"
-            onChange={(e) => setEmail(e.target.value)}
+            value= {signupInfo.email}
+            onChange={(e) => setSignupInfo(e.target.value)}
             className="form-control"
             id="email"
             aria-describedby="email"
@@ -39,14 +76,15 @@ function SignUpForm(props) {
           <label htmlFor="password">Password</label>
           <input
             type="password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setSignupInfo(e.target.value)}
             className="form-control"
             id="password"
             aria-describedby="password"
             placeholder="Password"
           />
         </div>
-      </Form>
+        <button>Sign up!</button>
+      </form>
     );
   };
 }
