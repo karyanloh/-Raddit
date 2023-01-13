ad9b9460e72f468eedaf226c3ab977b8420a72a5
Mackenzie Hill <hillmack92@gmail.com>
2023-01-13 13:33:49 -0300

    score updates when up/down arrow is clicked

--- ghi/src/PostScoring.js
+++ ghi/src/PostScoring.js
@@ -7,9 +7,23 @@ function PostScoring(props) {
     return (
         <div className="card post-scoring-card" >
             <ul className="list-group list-group-flush">
-                <button type="button" className="list-group-item list-group-item-primary list-group-item-action" disabled={props.upvoteUsers.includes(props.currentUserId)}>^</button>
+                <button
+                    onClick={() => setScore(score+1)}
+                    type="button"
+                    className="list-group-item list-group-item-primary list-group-item-action"
+                    disabled={props.upvoteUsers.includes(props.currentUserId)}
+                >
+                    ^
+                </button>
                 <li className="list-group-item">{score}</li>
-                <button type="button" className="list-group-item list-group-item-danger list-group-item-action" disabled={props.downvoteUsers.includes(props.currentUserId)}>v</button>
+                <button
+                    onClick={() => setScore(score-1)}
+                    type="button"
+                    className="list-group-item list-group-item-danger list-group-item-action"
+                    disabled={props.downvoteUsers.includes(props.currentUserId)}
+                >
+                    v
+                </button>
             </ul>
         </div>
     )
