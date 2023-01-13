9cfa5d317f387b63d3e0ab2a81e4c7c2788da63a
Mackenzie Hill <hillmack92@gmail.com>
2023-01-13 13:23:42 -0300

    read score from state

--- ghi/src/PostScoring.js
+++ ghi/src/PostScoring.js
@@ -1,11 +1,14 @@
+import {useState} from "react";
 
 // props for postscoring are being passed in mainpage
 function PostScoring(props) {
+    const [score, setScore] = useState(props.score)
+
     return (
         <div className="card post-scoring-card" >
             <ul className="list-group list-group-flush">
                 <button type="button" className="list-group-item list-group-item-primary list-group-item-action" disabled={props.upvoteUsers.includes(props.currentUserId)}>^</button>
-                <li className="list-group-item">{props.score}</li>
+                <li className="list-group-item">{score}</li>
                 <button type="button" className="list-group-item list-group-item-danger list-group-item-action" disabled={props.downvoteUsers.includes(props.currentUserId)}>v</button>
             </ul>
         </div>
