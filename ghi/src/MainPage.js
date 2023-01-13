c68004b5f0b1a06602c25832b73fce49d3d955d7
Mackenzie Hill <hillmack92@gmail.com>
2023-01-12 21:05:59 -0300

    logout button instead of link

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -3,10 +3,15 @@ function MainPage() {
         <div className="px-4 py-5 my-5 text-center">
             <h1 className="display-5 fw-bold">Raddit</h1>
             <div className="col-lg-6 mx-auto">
-            <p className="lead mb-4">
-                All things Rad(or bad)
-                Show Raddits
-            </p>
+                <p className="lead mb-4">
+                    <div>
+                        All things Rad(or bad)
+                    Show Raddits
+                    </div>
+                </p>
+                <div>
+
+                </div>
             </div>
         </div>
     );
