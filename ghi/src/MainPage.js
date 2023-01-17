a9c525a657a637fb81d76b9abff0948e4cd9da2f
stephanie <karyansloh@gmail.com>
2023-01-16 22:34:11 -0800

    added conditional rendering for login/logout buttons

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,10 +1,7 @@
 import { useEffect, useState } from "react";
 
 function MainPage(props) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   //   useEffect(() => function handleLoginClick(e) {});
-
   //   return (
   //     <>
   //       <div>
