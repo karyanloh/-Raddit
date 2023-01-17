f5161b0f9e2a0159255a12b2f188927d1c69b8d1
stephanie <karyansloh@gmail.com>
2023-01-16 23:42:56 -0800

    skeleton for mainpage complete will need to incorporate components into main page

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -5,33 +5,59 @@ function MainPage(props) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const { token } = useAuthContext();
 
-  return (
-    <div>
-      "test":
-      {token}
-    </div>
-  );
+  // return (
+  //   <div>
+  //     "test":
+  //     {token}
+  //   </div>
+  // );
 
   //   useEffect(() => function handleLoginClick(e) {});
-  //   return (
-  //     <>
-  //       <div>
-  //         {isLoggedIn ? (
-  //           <LogoutButton onClick={handleLogoutClick} />
-  //         ) : (
-  //           ((<LoginButton onClick={handleLoginClick} />),
-  //           (<SignUpButton onClick={handleSignupClick} />))
-  //         )}
-  //       </div>
-  //       <button>Login</button>
-  //       <div className="px-4 py-5 my-5 text-center">
-  //         <h1 className="display-5 fw-bold">Raddit</h1>
-  //         <div className="col-lg-6 mx-auto">
-  //           <p className="lead mb-4">All things Rad(or bad) Show Raddits</p>
-  //         </div>
-  //       </div>
-  //     </>
-  //   );
+  return (
+    <>
+      <div>
+        {/* {isLoggedIn ? (
+          <LogoutButton onClick={handleLogoutClick} />
+        ) : (
+          ((<LoginButton onClick={handleLoginClick} />),
+          (<SignUpButton onClick={handleSignupClick} />))
+        )} */}
+      </div>
+      <div className="navbar-buttons">
+        <button>Login</button>
+        <button>Sign Up</button>
+        <button>Logout</button>
+      </div>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Post title</h5>
+          <p className="card-text">Post description</p>
+          <a href="#" class="card-link">
+            Link to post detail
+          </a>
+          <a href="#" className="card-link">
+            Link to comments/post details
+          </a>
+          <div className="btn-group-vertical mb-3">
+            <button type="button" className="btn btn-outline-success">
+              Rad
+            </button>
+            <button type="button" className="btn btn-outline-danger">
+              Bad
+            </button>
+          </div>
+
+          <p className="vote-count">votes</p>
+        </div>
+      </div>
+      <div className="px-4 py-5 my-5 text-center">
+        <h1 className="display-5 fw-bold">Raddit</h1>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-4">All things Rad(or bad) Show Raddits</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MainPage;
