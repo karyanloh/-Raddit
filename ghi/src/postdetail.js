ce30f36e49412eccbef00f79b40a7b225bd54bf9
Jeffery Hebert <Jeffery.Hebert2@gmail.com>
2023-01-17 21:10:02 -0700

    detail page updates

--- ghi/src/postdetail.js
+++ ghi/src/postdetail.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "./utils";
 
+
+const api_url = 'http://localhost:8001/api/'
 function PostDetails() {
     const [post, setPost] = useState(null);
     const [comments, setComments] = useState(null);
@@ -13,21 +16,21 @@ function PostDetails() {
         description: description
     }
     const navigate = useNavigate();
-
+    const { token } = useAuthContext();
     useEffect(() => {
         async function fetchPost() {
             try {
-                const postUrl = `http://localhost:8001/api/post/${id}`;
+                const postUrl = `${api_url}post/${id}`;
                 const postResponse = await fetch(postUrl);
                 const postData = await postResponse.json();
                 setPost(postData);
 
-                const scoreUrl = `http://localhost:8001/api/post/postScore/${id}`;
+                const scoreUrl = `${api_url}post/postScore/${id}`;
                 const scoreResponse = await fetch(scoreUrl);
                 const scoreData = await scoreResponse.json();
                 setPost({ ...postData, score: scoreData.score });
 
-                const commentsUrl = `http://localhost:8001/api/comments/${id}`;
+                const commentsUrl = `${api_url}comments/${id}`;
                 const commentsResponse = await fetch(commentsUrl);
                 let commentData = await commentsResponse.json();
                 commentData = (Object.values(commentData))
@@ -46,13 +49,13 @@ function PostDetails() {
 
     async function put(e) {
         e.preventDefault()
-        const editUrl = `http://localhost:8001/api/post/${id}`;
+        const editUrl = `${api_url}post/${id}`;
                 const fetchConfig = {
                     method: "put",
                     body: JSON.stringify(edit),
                     headers: {
                         "Content-Type": "application/json",
-                        Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiOTllYmE3ZC0wN2ExLTQ4ZTktYWQ5NC1mMGZkMzE4YmM4NzUiLCJleHAiOjE2NzQwMTExMTQsInN1YiI6InRlc3QxMjMiLCJhY2NvdW50Ijp7ImVtYWlsIjoidGVzdEBnbWFpbC5jb20iLCJpZCI6IjYzYzczMTAzYjM3YjE1NDNmN2Q3MjM4OCIsInVzZXJuYW1lIjoidGVzdDEyMyJ9fQ.-Fg-vdZ-hhtAiDMJxZ9FxJwoWmggXxTzLZ0cWCjH4u8'}`,
+                        Authorization: `Bearer ${token}`,
                     }
                 }
         const editResponse = await fetch(editUrl, fetchConfig);
@@ -69,29 +72,31 @@ function PostDetails() {
             <div className="card-header">
                 <div className="d-flex justify-content-between">
                     <form onSubmit={put}>
-          <div className="form-group mb-4 ">
-            <label htmlFor="exampleFormControlTextarea1"></label>
-            <textarea
-              type="text"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              rows="10"
-              placeholder="Description"
-            />
-          </div>
-          <button className="btn btn-light">Create</button>
-        </form>
-              </div>
-            </div>
-        </div>
-
-
-
-    );
-    }
-
+              <div className="form-group mb-4 ">
+                <label htmlFor="exampleFormControlTextarea1"></label>
+                    <textarea
+                    type="text"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="10"
+                    placeholder="Description"
+                    />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Save</button>
+                    <button
+                    onClick={() => setIsEditing(false)}
+                    className="btn btn-secondary"
+                    >
+                    Cancel
+                    </button>
+                    </form>
+                    </div>
+                    </div>
+                    </div>
+                    );
+                    }
     if (!post) {
         return <div>Post not found</div>;
     }
@@ -106,19 +111,38 @@ function PostDetails() {
             <div className="card-header">
                 <div className="d-flex justify-content-between">
                     <div>
-                        <h6 className="font-weight-bold mb-0">{post.score}</h6>
-                        <p className="text-muted mb-0">points</p>
-                        <button onClick={setIsEditing}className="btn btn-light">Edit</button>
+                        <h6 className="card-title">{post.title}</h6>
+                        <p className="card-subtitle mb-2 text-muted">
+                            Score: {post.score}
+                        </p>
+                    </div>
+                    <div>
+                        <button className="btn btn-secondary mr-2">
+                            Upvote
+                        </button>
+                        <button className="btn btn-secondary">
+                            Downvote
+                        </button>
                     </div>
                 </div>
             </div>
-            <div className="card-header">
-              <div className="card-header">
-                  <h6 className="card-title">{post.title}</h6>
-                <div className="card-body">
-                  <p className="card-text">{post.description}</p>
+            <div className="card-body">
+                <p className="card-text">{post.description}</p>
+            </div>
+            <div className="card-footer">
+                <div className="d-flex justify-content-between">
+                    <div>
+                        <button onClick={setIsEditing} className="btn btn-secondary">Edit</button>
+                        <button className="btn btn-secondary">
+                            Delete
+                        </button>
+                    </div>
+                    <div>
+                        <p className="card-subtitle mb-2 text-muted">
+                            {comments[0].length} comments
+                        </p>
+                    </div>
                 </div>
-              </div>
             </div>
         </div>
         {comments[0].map((comment) => {
@@ -132,10 +156,6 @@ function PostDetails() {
                             })}
 
     </div>
-
-
-
     );
 }
-
 export default PostDetails;
