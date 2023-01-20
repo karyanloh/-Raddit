ae126cabb0162e3649e4e46979a75071a6a2096c
Jeffery Hebert <Jeffery.Hebert2@gmail.com>
2023-01-19 18:54:38 -0700

    updated main page

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -32,53 +32,40 @@ function MainPage() {
   return (
     <>
       <>
-        <div className="px-4 py-5 my-5 text-center">
+        <div className="px-4 py-5 my-5 text-center header">
           <h1 className="display-5 fw-bold">Raddit</h1>
           <div className="col-lg-6 mx-auto">
             <p className="lead mb-4">All things Rad(or bad) Show Raddits</p>
           </div>
         </div>
 
-        {post.map((p) => {
-          return (
-            <div className="card-body" key={p.id}>
-              <div>
-                <div className="card">
-                  <h5 className="card-title">{p.title}</h5>
-                  <a href="#">
-                    <p className="card-text">{p.description}</p>
-                  </a>
-                  <a href="#" className="card-link">
-                    Link to comments/post details
-                  </a>
+        <div className="container">
+          <div className="row">
+            {post.map((p) => {
+              return (
+                <div className="card-body col-md-4" key={p.id}>
+                  <div className="card">
+                    <h5 className="card-title">{p.title}</h5>
+                    <div className="btn-group-vertical mb-3">
+                      <button type="button" className="btn btn-outline-success">
+                        Rad
+                      </button>
+                      <button type="button" className="btn btn-outline-danger">
+                        Bad
+                      </button>
+                    </div>
+
+                    <p className="vote-count">votes</p>
+                    <a href={`/post/${p.id}`} className="card-link">
+                      Link to comments/post details
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
-
-        <div className="">
-          <h5 className="">Post title</h5>
-          <p className="card-text">Post description</p>
-          <a href="#" className="card-link">
-            Link to post detail
-          </a>
-          <a href="#" className="card-link">
-            Link to comments/post details
-          </a>
-          <div className="btn-group-vertical mb-3">
-            <button type="button" className="btn btn-outline-success">
-              Rad
-            </button>
-            <button type="button" className="btn btn-outline-danger">
-              Bad
-            </button>
+              );
+            })}
           </div>
-
-          <p className="vote-count">votes</p>
         </div>
       </>
-      {/* <div>"test2":{account}</div> */}
     </>
   );
       }
