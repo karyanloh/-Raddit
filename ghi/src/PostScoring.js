4a219020d306bf8a474a8a143f0d6f99da14f495
Randy Angel <randyangel84@gmail.com>
2023-01-19 17:46:34 -0700

    Functionable upvote and downvote

--- ghi/src/PostScoring.js
+++ ghi/src/PostScoring.js
@@ -8,7 +8,6 @@ function PostScoring(props) {
     // const [currentUserId, setCurrentUserId] = useState(props.currentUserId)
     let post_id = '63c9bb657463225907cde86b'
     async function handleUpArrowClick() {
-        console.log('activated')
         const url=`http://localhost:8001/api/postScore/upvote/${post_id}`;
             const fetchConfig ={
                 method: "put",
@@ -17,26 +16,19 @@ function PostScoring(props) {
             }
         }
         const scoreEditResponse = await fetch(url, fetchConfig);
-        console.log(scoreEditResponse)
         }
         
-    function handleDownArrowClick() {
-        // TODO: POST request to persist change in score(downvoteUsers array) when downarrow clicked
-        setScore(score-1)
-        setDownvoteUsers(downvoteUsers.concat(props.currentUserId))
-    }
-    // function handleUpArrowClick() {
-    //     // TODO: PUT request to persist change in score(upvoteUsers array) when uparrow clicked
-    //     setScore(score+1)
-    //     setUpvoteUsers(upvoteUsers.concat(props.currentUserId))
-    // }
+    async function handleDownArrowClick() {
+        const url=`http://localhost:8001/api/postScore/downvote/${post_id}`;
+            const fetchConfig ={
+                method: "put",
+                headers: {
+                    "Content-Type": "application/json",
+            }
+        }
+        const scoreEditResponse = await fetch(url, fetchConfig);
+        }
 
-    // function handleDownArrowClick() {
-    //     // TODO: PUT request to persist change in score(downvoteUsers array) when downarrow clicked
-    //     setScore(score-1)
-    //     setDownvoteUsers(downvoteUsers.concat(props.currentUserId))
-    // }
-}
 
     return (
         <div className="card post-scoring-card" >
@@ -45,7 +37,6 @@ function PostScoring(props) {
                     onClick={() => handleUpArrowClick()}
                     type="button"
                     className="list-group-item list-group-item-primary list-group-item-action"
-                    // disabled={props.upvoteUsers.includes(props.currentUserId)}
                     disabled={upvoteUsers.includes(props.currentUserId)}
                 >
                     ^
@@ -55,7 +46,6 @@ function PostScoring(props) {
                     onClick={() => handleDownArrowClick()}
                     type="button"
                     className="list-group-item list-group-item-danger list-group-item-action"
-                    // disabled={props.downvoteUsers.includes(props.currentUserId)}
                     disabled={downvoteUsers.includes(props.currentUserId)}
                 >
                     v
