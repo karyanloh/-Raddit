fe2fc3e2cf32d3c78fabe32184bb4a472ecb7b44
Jeffery Hebert <Jeffery.Hebert2@gmail.com>
2023-01-20 19:29:41 -0700

    Updated queries

--- content-service/content_queries.py
+++ content-service/content_queries.py
@@ -2,12 +2,13 @@ import os
 import pymongo
 from bson import ObjectId
 
-dbuser = os.environ["MONGO_USER"]
-dbpass = os.environ["MONGO_PASSWORD"]
-dbhost = os.environ["WAIT_HOSTS"]
+# dbuser = os.environ["MONGO_USER"]
+# dbpass = os.environ["MONGO_PASSWORD"]
+# dbhost = os.environ["MONGO_HOST"]
 mongodb = os.environ["DATABASE_NAME"]
 
-mongo_str = f"mongodb://{dbuser}:{dbpass}@{dbhost}"
+# mongo_str = f"mongodb://{dbuser}:{dbpass}@{dbhost}"
+mongo_str = os.environ["DATABASE_URL"]
 
 client = pymongo.MongoClient(mongo_str)
 
