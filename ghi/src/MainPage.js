e01fe3fbd029de8626697f60b52f4d1353978648
Jeffery Hebert <Jeffery.Hebert2@gmail.com>
2023-01-20 19:40:33 -0700

    Testing FE deployment

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,62 +1,65 @@
-import { useEffect, useState } from "react";
-import "./index.css";
+// import { useEffect, useState } from "react";
+// import "./index.css";
 
 function MainPage() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [post, setPost] = useState({});
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [post, setPost] = useState({});
 
-  useEffect(() => {
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
-  async function getData() {
-    try {
-      const postUrl = `http://localhost:8001/api/posts/`;
-      const postResponse = await fetch(postUrl);
-      const postData = await postResponse.json();
-      setPost(postData.posts);
-      setIsLoading(false);
-    } catch (error) {
-      console.error(error);
-      setPost({ error: "Error fetching post" });
-    }
-  }
-  if (isLoading) {
-    return (
-      <div className="spinner-border" animation="border" variant="primary" />
-    );
-  } else {
-    return (
-      <>
-        {post.map((p) => {
-          return (
-            <div className=" card-group text-blue ml-3 mb-8" key={p.id}>
-              {/* <div className="card"> */}
-              <div className="btn-group-vertical mb-3 ">
-                <button
-                  type="button"
-                  className="btn btn-outline-success text-left"
-                >
-                  Rad
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-outline-danger text-left"
-                >
-                  Bad
-                </button>
-              </div>
-              <div className="post">
-                <a href={`/post/${p.id}`} className="card-link">
-                  <p className="card-title">{p.title}</p>
-                </a>
-              </div>
-            </div>
-          );
-        })}
-      </>
-    );
-  }
+  // async function getData() {
+  //   try {
+  //     const postUrl = `http://localhost:8001/api/posts/`;
+  //     const postResponse = await fetch(postUrl);
+  //     const postData = await postResponse.json();
+  //     setPost(postData.posts);
+  //     setIsLoading(false);
+  //   } catch (error) {
+  //     console.error(error);
+  //     setPost({ error: "Error fetching post" });
+  //   }
+  // }
+  // if (isLoading) {
+  //   return (
+  //     <div className="spinner-border" animation="border" variant="primary" />
+  //   );
+  // } else {
+  //   return (
+  //     <>
+  //       {post.map((p) => {
+  //         return (
+  //           <div className=" card-group text-blue ml-3 mb-8" key={p.id}>
+  //             {/* <div className="card"> */}
+  //             <div className="btn-group-vertical mb-3 ">
+  //               <button
+  //                 type="button"
+  //                 className="btn btn-outline-success text-left"
+  //               >
+  //                 Rad
+  //               </button>
+  //               <button
+  //                 type="button"
+  //                 className="btn btn-outline-danger text-left"
+  //               >
+  //                 Bad
+  //               </button>
+  //             </div>
+  //             <div className="post">
+  //               <a href={`/post/${p.id}`} className="card-link">
+  //                 <p className="card-title">{p.title}</p>
+  //               </a>
+  //             </div>
+  //           </div>
+  //         );
+  //       })}
+  //     </>
+  //   );
+  // }
+  return (
+    <p>Hello World</p>
+  )
 }
 
 export default MainPage;
--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,143 +1,143 @@
-import { NavLink, Link } from "react-router-dom";
-import "./index.css";
-import raddit from "./raddit_new.png";
-import { useToken } from "./utils";
+// import { NavLink, Link } from "react-router-dom";
+// import "./index.css";
+// import raddit from "./raddit_new.png";
+// import { useToken } from "./utils";
 
-function Nav() {
-  const [token, Login, logout] = useToken();
-  console.log(Login)
-  return (
-    <>
-      <nav className="nav-bg h-18 p-3 mb-2 navbar navbar-expand-lg navbar-dark text-dark">
-        <div className="d-flex flex-column justify-content-center align-items-center"></div>
-        <div className="container-fluid ">
-          <img src={raddit} alt="Logo" className="pr-2" />
-          <h1 className="text-black">raddit</h1>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item ms-auto">
-              {!token && (
-                <NavLink className="nav-link text-white" to="/signup">
-                  SignUp
-                </NavLink>
-              )}
-            </li>
-            <li className="nav-item">
-              {!token && (
-                <NavLink className="nav-link text-white" to="/login">
-                  Login
-                </NavLink>
-              )}
-            </li>
+// function Nav() {
+//   const [token, Login, logout] = useToken();
+//   console.log(Login)
+//   return (
+//     <>
+//       <nav className="nav-bg h-18 p-3 mb-2 navbar navbar-expand-lg navbar-dark text-dark">
+//         <div className="d-flex flex-column justify-content-center align-items-center"></div>
+//         <div className="container-fluid ">
+//           <img src={raddit} alt="Logo" className="pr-2" />
+//           <h1 className="text-black">raddit</h1>
+//           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//             <li className="nav-item ms-auto">
+//               {!token && (
+//                 <NavLink className="nav-link text-white" to="/signup">
+//                   SignUp
+//                 </NavLink>
+//               )}
+//             </li>
+//             <li className="nav-item">
+//               {!token && (
+//                 <NavLink className="nav-link text-white" to="/login">
+//                   Login
+//                 </NavLink>
+//               )}
+//             </li>
 
-            {/* TODO: hide logout if not logged in */}
+//             {/* TODO: hide logout if not logged in */}
 
-            <li className="nav-item">
-              {token && (
-                <span
-                  onClick={() => {
-                    logout();
-                  }}
-                  role={"button"}
-                  className="nav-link"
-                >
-                  Logout
-                </span>
-              )}
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <nav className="subnav w-100 h-10 p-3 shadow mb-5 navbar navbar-expand-sm ">
-        <div className="collapse navbar-collapse " id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink
-                className="nav-link text-dark"
-                aria-current="page"
-                to="/"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item dropdown">
-              <Link
-                className="nav-link dropdown-toggle text-dark"
-                id="navbarDropdown"
-                data-bs-toggle="dropdown"
-                to="/"
-              >
-                MySubRaddits
-              </Link>
-              <ul className="dropdown-menu">
-                <li>
-                  <NavLink
-                    className="dropdown-item"
-                    onClick={() => {
-                      window.location.href = "/Music";
-                    }}
-                  >
-                    Music
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    className="dropdown-item"
-                    onClick={() => {
-                      window.location.href = "/Movies";
-                    }}
-                  >
-                    Movies
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    className="dropdown-item"
-                    onClick={() => {
-                      window.location.href = "/Tech";
-                    }}
-                  >
-                    Technology
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    className="dropdown-item"
-                    onClick={() => {
-                      window.location.href = "/Sports";
-                    }}
-                  >
-                    Sports
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    className="dropdown-item"
-                    onClick={() => {
-                      window.location.href = "/Miscellaneous";
-                    }}
-                  >
-                    Miscellaneous
-                  </NavLink>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link text-dark" to="/post/new">
-                Create Post
-              </NavLink>
-            </li>
-            {/* <li className="nav-item">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-              </li> */}
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
-}
-export default Nav;
+//             <li className="nav-item">
+//               {token && (
+//                 <span
+//                   onClick={() => {
+//                     logout();
+//                   }}
+//                   role={"button"}
+//                   className="nav-link"
+//                 >
+//                   Logout
+//                 </span>
+//               )}
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//       <nav className="subnav w-100 h-10 p-3 shadow mb-5 navbar navbar-expand-sm ">
+//         <div className="collapse navbar-collapse " id="navbarSupportedContent">
+//           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//             <li className="nav-item">
+//               <NavLink
+//                 className="nav-link text-dark"
+//                 aria-current="page"
+//                 to="/"
+//               >
+//                 Home
+//               </NavLink>
+//             </li>
+//             <li className="nav-item dropdown">
+//               <Link
+//                 className="nav-link dropdown-toggle text-dark"
+//                 id="navbarDropdown"
+//                 data-bs-toggle="dropdown"
+//                 to="/"
+//               >
+//                 MySubRaddits
+//               </Link>
+//               <ul className="dropdown-menu">
+//                 <li>
+//                   <NavLink
+//                     className="dropdown-item"
+//                     onClick={() => {
+//                       window.location.href = "/Music";
+//                     }}
+//                   >
+//                     Music
+//                   </NavLink>
+//                 </li>
+//                 <li>
+//                   <NavLink
+//                     className="dropdown-item"
+//                     onClick={() => {
+//                       window.location.href = "/Movies";
+//                     }}
+//                   >
+//                     Movies
+//                   </NavLink>
+//                 </li>
+//                 <li>
+//                   <NavLink
+//                     className="dropdown-item"
+//                     onClick={() => {
+//                       window.location.href = "/Tech";
+//                     }}
+//                   >
+//                     Technology
+//                   </NavLink>
+//                 </li>
+//                 <li>
+//                   <NavLink
+//                     className="dropdown-item"
+//                     onClick={() => {
+//                       window.location.href = "/Sports";
+//                     }}
+//                   >
+//                     Sports
+//                   </NavLink>
+//                 </li>
+//                 <li>
+//                   <NavLink
+//                     className="dropdown-item"
+//                     onClick={() => {
+//                       window.location.href = "/Miscellaneous";
+//                     }}
+//                   >
+//                     Miscellaneous
+//                   </NavLink>
+//                 </li>
+//               </ul>
+//             </li>
+//             <li className="nav-item">
+//               <NavLink className="nav-link text-dark" to="/post/new">
+//                 Create Post
+//               </NavLink>
+//             </li>
+//             {/* <li className="nav-item">
+//                 <input
+//                   className="form-control me-2"
+//                   type="search"
+//                   placeholder="Search"
+//                   aria-label="Search"
+//                 />
+//               </li> */}
+//           </ul>
+//         </div>
+//       </nav>
+//     </>
+//   );
+// }
+// export default Nav;
