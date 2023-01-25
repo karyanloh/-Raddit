a1b220e3c0f6a534c44a96f42c01b6870cef9d84
minyue <yukilmy520@gmail.com>
2023-01-24 19:06:01 -0800

    reformatted one file

--- user-service/tests/yuki_test.py
+++ user-service/tests/yuki_test.py
@@ -6,7 +6,9 @@ client = TestClient(app)
 
 
 def test_get_token_returns_none_for_user_not_logged_in():
-    app.dependency_overrides[authenticator.try_get_current_account_data] = lambda: None
+    app.dependency_overrides[
+        authenticator.try_get_current_account_data
+    ] = lambda: None
     response = client.get("/token")
     app.dependency_overrides = {}
     assert response.status_code == 200
@@ -15,14 +17,16 @@ def test_get_token_returns_none_for_user_not_logged_in():
 
 def test_get_token_returns_token_for_user_logged_in():
     user = {
-        "id": '1',
+        "id": "1",
         "email": "yuki@gmail.com",
         "username": "yuki520",
     }
     app.dependency_overrides[
         authenticator.try_get_current_account_data
     ] = lambda: user
-    response = client.get("/token", cookies={authenticator.cookie_name: "HELLO!"})
+    response = client.get(
+        "/token", cookies={authenticator.cookie_name: "HELLO!"}
+    )
     app.dependency_overrides = {}
     assert response.status_code == 200
     data = response.json()
