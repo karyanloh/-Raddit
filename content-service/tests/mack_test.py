de894b67b94c6205a5b9aa10ded772406dec0f35
Mackenzie Hill <hillmack92@gmail.com>
2023-01-26 16:06:41 -0300

    updates to mack test

--- content-service/tests/mack_test.py
+++ content-service/tests/mack_test.py
@@ -1,4 +1,6 @@
 from fastapi.testclient import TestClient
+
+from auth import authenticator
 from main import app
 from routers.score import PostScoreOut
 from content_queries import ContentQueries
@@ -12,6 +14,12 @@ post_score_out = PostScoreOut(
 )
 
 
+class MockUser:
+    user = {
+        "user_id": "1a",
+    }
+
+
 class MockScore:
     def get_post_score_by_post_id(
         self,
@@ -19,10 +27,23 @@ class MockScore:
     ):
         return post_score_out
 
+    def increase_post_score_by_id(self, post_id, user_id):
+        return post_score_out
+
 
 def test_get_post_score_by_post_id():
     app.dependency_overrides[ContentQueries] = MockScore
     response = client.get("/api/post/postScore/1")
+    assert response.status_code == 200
+    assert response.json() == post_score_out
+
+
+def test_increase_post_score_by_id():
+    overrides = authenticator.try_get_current_account_data
+    app.dependency_overrides[overrides] = lambda: None
+    response = client.get(get)
+    app.dependency_overrides[ContentQueries] = MockScore
+    response = client.put("/api/postScore/upvote/1/1a")
     assert response.status_code == 200
     assert response.json() == post_score_out
 
